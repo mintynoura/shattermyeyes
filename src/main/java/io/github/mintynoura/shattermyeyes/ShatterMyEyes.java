ecf6f597a1160c39c53429c9ea6efa6d1f8e9e26
mintynoura <mintynoura@gmail.com>
2026-02-04 16:31:14 +0300

    1.20

--- src/main/java/io/github/mintynoura/shattermyeyes/ShatterMyEyes.java
+++ src/main/java/io/github/mintynoura/shattermyeyes/ShatterMyEyes.java
@@ -4,15 +4,12 @@ import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ShatterMyEyes implements ModInitializer {
 	public static final String MOD_ID = "shattermyeyes";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final Identifier SOUND_ID = Identifier.fromNamespaceAndPath(MOD_ID, "entity.ender_eye.shatter");
+	public static final ResourceLocation SOUND_ID = new ResourceLocation(MOD_ID, "entity.ender_eye.shatter");
 
 	public static final SoundEvent ENDER_EYE_SHATTER = Registry.register(BuiltInRegistries.SOUND_EVENT, SOUND_ID, SoundEvent.createVariableRangeEvent(SOUND_ID));
 
